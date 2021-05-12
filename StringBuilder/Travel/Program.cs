28e5babc18422c250a94e0f7dfcefd2cb09b010a
Ederson Furlan <edersonmello@msn.com>
2021-05-12 19:49:25 -0300

    StringBuilder

---
+++ StringBuilder/Travel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Travel.Entities;
+
+namespace Travel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow that's awesome!");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018"),
+                "Travelling to New Zealand",
+                "I'm gonna visit this wonderful country!",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night!");
+            Comment c4 = new Comment("May the force be with you");
+            Post p2 = new Post(
+                DateTime.Parse("10/05/2021 16:40:35"),
+                "Good night guys.",
+                "See you tomorrow",
+                5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
