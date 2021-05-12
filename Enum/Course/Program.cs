ad8ceec39af4ffba9adf04e1cbadd9224f39d130
Ederson Furlan <edersonmello@msn.com>
2021-05-12 19:48:12 -0300

    Enum

---
+++ Enum/Course/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Course.Entities;
+using Course.Entities.Enums;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+        }
+    }
+}
