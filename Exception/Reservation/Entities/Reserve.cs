22a86150a2f2b41dd73176926e5895d0e442d305
Ederson Furlan <edersonmello@msn.com>
2021-08-10 08:46:20 -0300

    exception exercises

---
+++ Exception/Reservation/Entities/Reserve.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Reservation.Entities.Exceptions;
+
+namespace Reservation.Entities
+{
+    class Reserve
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reserve()
+        {
+        }
+
+        public Reserve(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+                throw new DomainException("Reservation date for update must be future dates");
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+
+            if (checkIn < now || checkOut < now)
+                throw new DomainException("Reservation date for update must be future dates");
+
+            if (checkOut <= checkIn)
+                throw new DomainException("Reservation date for update must be future dates");
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room"
+                   + RoomNumber
+                   + ", check-in: "
+                   + CheckIn.ToString("dd/MM/yyyy")
+                   + ", check-out: "
+                   + CheckOut.ToString("dd/MM/yyyy")
+                   + ", "
+                   + Duration()
+                   + " nights";
+        }
+    }
+}
---
+++ Exception/Reservation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Reservation.Entities;
+using Reservation.Entities.Exceptions;
+
+namespace Reservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-In date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+                Reserve reservation = new Reserve(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
