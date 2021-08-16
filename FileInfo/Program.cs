f805403666f1f452f552233ad948ae7c61294d6a
Ederson <ederson.furlan@ubs.com>
2021-08-16 09:00:02 -0300

    FileInfo

---
+++ FileInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInfoIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                    Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.Write("An error ocurred: " + e.Message);
+                throw;
+            }
+        }
+    }
+}
