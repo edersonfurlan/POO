6aa13ce3d436c485b9ea90b827f29b0bdb3924ed
Ederson <ederson.furlan@ubs.com>
2021-08-24 09:04:29 -0300

    add interfaces

---
+++ Files/Directory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                    Console.WriteLine(s);
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                    Console.WriteLine(s);
+
+                Directory.CreateDirectory(path + @"\newfolder");
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occured: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Interfaces/AluguelCarroSemInterface/Entities/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace AluguelCarroSemInterface.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic Payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTex: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotal payment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
