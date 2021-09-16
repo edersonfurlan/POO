ef162e7afa5ab9346921c199b3c3b1d6378db44a
Ederson <ederson.furlan@ubs.com>
2021-09-16 17:58:07 -0300

    Dictionary exercises

---
+++ GenericsSetDictionary/ExerciseProposedDictionary/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExerciseProposedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> candidate = new Dictionary<string, int>();
+
+            string path = @"c:\temp\votes.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+
+                        if (candidate.ContainsKey(name))
+                            candidate[name] += votes;
+                        else
+                            candidate[name] = votes;
+                    }
+                }
+
+                Console.WriteLine("Votes:");
+
+                foreach (KeyValuePair<string, int> item in candidate)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
