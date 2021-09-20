a8116326259f89e5cfd067ad5412da1ef6cfba8e
Ederson <ederson.furlan@ubs.com>
2021-09-20 09:08:04 -0300

    extension methods/comparision/delegates

---
+++ LambdaDelegatesLINQ/Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using Delegates.Entities;
+using System;
+
+namespace Delegates
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            //por ser type safety o delegate BinaryNumericOperation só pode receber dois doubles e retornar outro double, como indicado na linha 6
+            //Max e Sum funcionam, Square não
+
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            // double result = op(a, b);
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            double result = op.Invoke(a, b);
+            
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ LambdaDelegatesLINQ/LambdaDelegatesLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaDelegatesLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
